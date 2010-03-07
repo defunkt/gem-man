d1d1238ffec002cefe0126b4f9ebf728182c0979
Chris Wanstrath <chris@ozmm.org>
2010-03-06 16:07:16 -0800

    no ron yet

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,10 +1,4 @@
 require 'rubygems/command_manager'
 require 'rubygems/gem/specification'
 
-require 'ron'
-
-# require 'rubygems/command'
-# require 'rubygems/dependency'
-# require 'rubygems/version_option'
-
 Gem::CommandManager.instance.register_command :man
