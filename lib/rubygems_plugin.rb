8b2c563000b566ba6995b2161729c51df73be688
Rémy Coutable <rymai@rymai.me>
2011-08-13 00:32:49 +0200

    Update .gemspec and replace double quotes with single quote (style matters!!).

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,5 +1,5 @@
 require 'rubygems/command_manager'
-require "rubygems/version_option"
+require 'rubygems/version_option'
 require 'rubygems/gem/specification'
 
 Gem::CommandManager.instance.register_command :man
